2b523b2bc3b1b100054300d7577eb71cb4dbf545
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-02-19 18:39:56 +0300

    Added FluentValidation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constans;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,19 +23,11 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
             //business codes
 
-            if (DateTime.Now.Hour == 19)
-            {
-                return new ErrorResult(Messages.MaintenanceTime);
-            }
-
-            if (car.DailyPrice < 0)
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);
-            }
 
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(150).When(c => c.BrandId == 2).WithMessage("Brand Id si 2 olan araçlara ait günlük ücret en az 150 olabilir.");
+        }
+
+        
+    }
+}
